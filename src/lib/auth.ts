a66cdf02574693494e038559a04b2474481ee0ba
ayushsingh-ayushsingh <ayushpno@gmail.com>
2026-03-19 08:46:42 +0530

    Project Setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+
+export const auth = betterAuth({
+  baseURL: "http://localhost:3000/",
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+});
